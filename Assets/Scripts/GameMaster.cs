7f77d64beed5d1389dc654f7132e12b9b4a97792
kaichi <satokai0124@gmail.com>
2020-05-06 19:15:44 +0900

    add destroy brokenboxes and wallout to result scene.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+
+  public int boxNum;
+  public float nowTime;
+  void Start()
+  {
+    nowTime = 0;
+  }
+
+  void Update()
+  {
+    nowTime += Time.deltaTime;
+    if (boxNum <= 0)
+    {
+      GameOver();
+    }
+  }
+
+  public void GameOver()
+  {
+    SceneManager.LoadScene("Result");
+  }
+}
---
+++ Assets/Scripts/WallOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallOut : MonoBehaviour
+{
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    GameObject.Find("Master").GetComponent<GameMaster>().GameOver();
+  }
+}
